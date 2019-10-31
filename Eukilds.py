edc537812db3d3b68c23c6a05b9219df378a1394
Lizator <frederikkoefoeds@gmail.com>
2019-10-31 17:34:29 +0100

    adding .gitignore and LaTeX code output

--- Eukilds.py
+++ Eukilds.py
@@ -1,5 +1,7 @@
 import math
 
+# Python solution to Eukilds algorithm for Diskrete Mathematics with automated LaTeX code output
+
 # does the computing for Eukilds algorithm
 class Eukild:
 
@@ -16,8 +18,8 @@ class Eukild:
         pass
 
 
-    def gcp(self, a, b):
-        myTable = tableSetUp()
+    def gcd(self, a, b, addLatex):
+        myTable = TableSetUp()
 
         if a < b:
             self.update(0, 1, 1, 0, b, a)
@@ -29,15 +31,27 @@ class Eukild:
             myTable.addLine(-1, self.s0, self.t0, " ", self.r0)
             myTable.addLine(0, self.s1, self.t1, " ", self.r1)
 
+        if addLatex:
+            self.tex = Latex()
+            self.tex.beginTabluar(self.r0, self.r1)
+
         i = 1
         while (self.r1 != 0):
             q = math.floor(self.r0 / self.r1)
             s2 = self.s0 - (self.s1 * q)
             t2 = self.t0 - (self.t1 * q)
             r2 = self.r0 - (self.r1 * q)
+
             myTable.addLine(i, s2, t2, q, r2)
+
+            if addLatex:
+                self.tex.addLineAsCode(i, s2, t2, q, r2)
+
             self.update(self.s1, s2, self.t1, t2, self.r1, r2)
             i += 1
+
+        if addLatex:
+            self.tex.endTabular()
 
         print(myTable.table)
 
@@ -48,15 +62,27 @@ class Latex:
     def __init__(self):
         pass
 
-    def beginTabluar(self):
+    def beginTabluar(self, a, b):
+        name = "LaTeX/Tabular gcd(" + str(a) + ", " + str(b) + ")"
+        self.f = open(name, "w")
+        startTxt = "gcd(" + str(a) + ", " + str(b) + ")" + """
+\\begin{tabular}{c|c c c c} 
+    i & $s_i$ & $t_i$ & $q_i$ & $r_i$ \\\\ \\hline \n"""
+
+        self.f.write(startTxt)
 
 
-    def toCode(self):
-        return
+    def addLineAsCode(self, i, s, t, q, r):
+        line = "    " + str(i) + " & " + str(s) + " & " + str(t) + " & " + str(q) + " & " + str(r) + "\\\\ \n"
+        self.f.write(line)
+
+    def endTabular(self):
+        self.f.write("\\end{tabular}")
+        self.f.close()
 
 
 # turns values to a tableLine
-class txt:
+class Txt:
     def __init__(self):
         pass
 
@@ -66,9 +92,9 @@ class txt:
 
 
 # generates table
-class tableSetUp:
+class TableSetUp:
     def __init__(self):
-        self.myTxt = txt()
+        self.myTxt = Txt()
         self.table = """i | s   t   q   t
 ------------------"""
 
@@ -79,11 +105,11 @@ class tableSetUp:
 eu = Eukild()
 
 
-eu.gcp(4,3)
+eu.gcd(4, 3, False)
 
 print (" ")
 
-eu.gcp(341,217)
+eu.gcd(341, 217, True)
 
 
 
